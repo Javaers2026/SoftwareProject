71359f1c535a38fd82357e8a575082d3063c3ecd
Ayman Yaish <Aymanyaish05@gmail.com>
2026-04-11 13:37:12 +0300

    Notifications & Mocking updated to send to the Operating Sysetm

--- src/main/java/com/project/observer/NotifacationManager.java
+++ src/main/java/com/project/observer/NotifacationManager.java
@@ -1,20 +1,15 @@
 package com.project.observer;
 
 import com.project.User;
-import java.util.ArrayList;
-import java.util.List;
 
-public class NotifacationManager {
-    
-    private List<Observer> observers = new ArrayList<>();
-    
-    public void addObserver(Observer observer){
-        observers.add(observer);
+public class NotifacationManager extends Subject{
+
+    public NotifacationManager() {
+        addObserver(new EmailNotifacation());
+        addObserver(new SystemNotification());
     }
-
+    
     public void notifyAll(User user, String message){
-        for(Observer observer : observers){
-            observer.notify(user, message);
-        }
+        notifyObservers(user, message);
     }
 }
--- src/main/java/com/project/observer/Subject.java
+++ src/main/java/com/project/observer/Subject.java
@@ -1,5 +1,23 @@
 package com.project.observer;
 
-public class Subject {
-    
+import com.project.User;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(User user, String message) {
+        for (Observer observer : observers) {
+            observer.notify(user, message);
+        }
+    }
 }
